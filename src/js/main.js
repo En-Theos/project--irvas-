fba5ecbe09aae0bf9955db331459c5049b7d704e
EnTheos <theosenigma@gmail.com>
2021-07-26 22:35:39 +0300

    Adding tabs

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,11 @@
+import './slider';
 import modal from "./modules/modal";
+import tabs from "./modules/tabs";
 
 window.addEventListener('DOMContentLoaded', () => {
     modal(".popup_engineer_btn", ".popup_engineer");
     modal(".contact_us_wrap", ".popup");
     modal(".feedback_block .phone_link", ".popup");
+    tabs(".glazing_slider", ".glazing_block", ".glazing_content", "active");
+    tabs(".decoration_slider", ".no_click", ".decoration_content > div > div", "after_click");
 });
